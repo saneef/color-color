bb0cff296f47ab90f58fb7576c26a19f31a96249
saneef <hello@saneef.com>
2020-03-26 15:35:30 +0530

    build(rollup): generates hash in asset filenames
    Prettier changed from tab seperated to space

--- postcss.config.js
+++ postcss.config.js
@@ -1,24 +1,17 @@
-// postcss.config.js
 const purgecss = require("@fullhuman/postcss-purgecss")({
-	// Specify the paths to all of the template files in your project
-	content: [
-		"./src/**/*.html",
-		"./src/**/*.svelte",
-		// etc.
-	],
-
-	// Include any special characters you're using in this regular expression
-	defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || [],
+  content: ["./src/**/*.svelte"],
+  whitelistPatterns: [/svelte-/],
+  defaultExtractor: content => content.match(/[A-Za-z0-9-_:/]+/g) || [],
 });
 
 const production = !process.env.ROLLUP_WATCH;
 
 module.exports = {
-	plugins: [
-		require("postcss-import")(),
-		require("tailwindcss"),
-		require("autoprefixer"),
-		require("postcss-nesting"),
-		...(production ? [purgecss, require("cssnano")] : []),
-	],
+  plugins: [
+    require("postcss-import")(),
+    require("tailwindcss"),
+    require("autoprefixer"),
+    require("postcss-nesting"),
+    ...(production ? [purgecss, require("cssnano")] : []),
+  ],
 };

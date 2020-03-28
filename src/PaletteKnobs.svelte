2bdaa7557dbea7ef02560e686765493c568bff8e
saneef <hello@saneef.com>
2020-03-28 15:22:42 +0530

    fix(palette-knobs): adds index to color palettes

--- src/PaletteKnobs.svelte
+++ src/PaletteKnobs.svelte
@@ -1,10 +1,34 @@
 <style>
   .root {
-    @apply p-4 -mx-4 my-4 border-t-4 border-b-4 border-gray-900;
+    @apply p-4 pt-3 -mx-4 my-4 border-t-4 border-b-4 border-gray-900;
   }
 
   h2 {
     @apply text-lg font-bold;
+  }
+
+  .index {
+    @apply flex mb-4 -mx-1;
+  }
+
+  .index li {
+    @apply mx-1;
+  }
+
+  .index-marker {
+    @apply flex justify-center items-center h-8 w-8 no-underline border-2 border-gray-900 rounded-full;
+  }
+
+  .index-marker--active {
+    @apply relative text-white bg-gray-900;
+  }
+
+  .index-marker--active::before {
+    @apply block absolute w-1 h-4 bg-gray-900;
+    bottom: 100%;
+    left: 50%;
+    margin-left: -0.125rem;
+    content: "";
   }
 </style>
 
@@ -18,11 +42,28 @@
   const easeOptions = Object.keys(eases);
 
   let current = 0;
+  let paletteIndex = [];
 
   $: current = $paletteParams.current;
+  $: paletteIndex = $paletteParams.params.map((_, i) => i);
 </script>
 
 <div class="root">
+  <ul class="index">
+    {#each paletteIndex as pi, i (i)}
+      <li>
+        <a
+          class="index-marker"
+          class:index-marker--active="{current === i}"
+          href="#{i + 1}"
+          on:click|preventDefault="{() => {
+            $paletteParams.current = i;
+          }}">
+          {i + 1}
+        </a>
+      </li>
+    {/each}
+  </ul>
   <ControlGroup>
     <h2>Hue</h2>
     <RangeField

a2f936a1ae5bbb7d0492265b72f4f74b4bb41503
saneef <hello@saneef.com>
2020-03-28 14:03:30 +0530

    feat(control-group): adds title support

--- src/ControlGroup.svelte
+++ src/ControlGroup.svelte
@@ -2,8 +2,20 @@
   .control-group + .control-group {
     @apply mt-4 pt-4 border-t border-gray-500;
   }
+
+  .control-group__title {
+    @apply text-lg font-bold;
+  }
 </style>
 
+<script>
+  export let title = null;
+  export let titleId;
+</script>
+
 <div class="control-group">
+  {#if title}
+    <h2 id="{titleId}" class="control-group__title">{title}</h2>
+  {/if}
   <slot />
 </div>

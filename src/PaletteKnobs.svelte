08c7a8ebb4aaea0e634695dfc03be0c18e17c6e8
saneef <hello@saneef.com>
2020-03-31 00:11:22 +0530

    refactor(palette-selector): move code to separate component

--- src/PaletteKnobs.svelte
+++ src/PaletteKnobs.svelte
@@ -2,68 +2,24 @@
   .root {
     @apply p-4 pt-3 -mx-4 my-4 border-t-4 border-b-4 border-gray-900;
   }
-
-  h2 {
-    @apply text-lg font-bold;
-  }
-
-  .index {
-    @apply flex mb-4 -mx-1;
-  }
-
-  .index li {
-    @apply mx-1;
-  }
-
-  .index-marker {
-    @apply flex justify-center items-center h-8 w-8 no-underline border-2 border-gray-900 rounded-full;
-  }
-
-  .index-marker--active {
-    @apply relative text-white bg-gray-900;
-  }
-
-  .index-marker--active::before {
-    @apply block absolute w-1 h-4 bg-gray-900;
-    bottom: 100%;
-    left: 50%;
-    margin-left: -0.125rem;
-    content: "";
-  }
 </style>
 
 <script>
+  import ControlGroup from "./ControlGroup.svelte";
+  import PaletteSelector from "./PaletteSelector.svelte";
+  import RangeField from "./RangeField.svelte";
+  import SelectField from "./SelectField.svelte";
   import { headerCase } from "change-case";
   import { paletteParams, config } from "./store";
-  import SelectField from "./SelectField.svelte";
-  import RangeField from "./RangeField.svelte";
-  import ControlGroup from "./ControlGroup.svelte";
 
   let current = 0;
-  let paletteIndex = [];
 
   $: current = $paletteParams.paletteIndex;
-  $: paletteIndex = $paletteParams.params.map((_, i) => i);
 </script>
 
 <div class="root">
-  <ul class="index">
-    {#each paletteIndex as pi, i (i)}
-      <li>
-        <a
-          class="index-marker"
-          class:index-marker--active="{current === i}"
-          href="#{i + 1}"
-          on:click|preventDefault="{() => {
-            $paletteParams.paletteIndex = i;
-          }}">
-          {i + 1}
-        </a>
-      </li>
-    {/each}
-  </ul>
-  <ControlGroup>
-    <h2>Hue</h2>
+  <PaletteSelector />
+  <ControlGroup title="Hue">
     <RangeField
       id="hue-start"
       label="Start"
@@ -88,8 +44,7 @@
     </SelectField>
   </ControlGroup>
 
-  <ControlGroup>
-    <h2>Saturation</h2>
+  <ControlGroup title="Saturation">
     <RangeField
       id="sat-start"
       label="Start"
@@ -115,8 +70,7 @@
     </SelectField>
   </ControlGroup>
 
-  <ControlGroup>
-    <h2>Lightness</h2>
+  <ControlGroup title="Lightness">
     <RangeField
       id="lig-start"
       label="Start"

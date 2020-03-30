e04399de86275a7b4afb7c425a77fdf5ac262193
saneef <hello@saneef.com>
2020-03-31 03:00:45 +0530

    fix(store): swatchIndex goes undefined when steps goes below current swatchIndex
    Moves steps inside paletParams store. Then, check and updates swatchIndex when steps value is updated.

--- src/StepsKnob.svelte
+++ src/StepsKnob.svelte
@@ -1,5 +1,5 @@
 <script>
-  import { steps } from "./store";
+  import { paletteParams } from "./store";
   import ControlGroup from "./ControlGroup.svelte";
   import RangeField from "./RangeField.svelte";
 </script>
@@ -8,7 +8,7 @@
   <RangeField
     id="steps-range"
     labelledby="steps-title"
-    bind:value="{$steps}"
+    bind:value="{$paletteParams.steps}"
     min="3"
     max="21" />
 </ControlGroup>

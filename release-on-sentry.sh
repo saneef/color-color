75ea34b263f818ae0a6d77c4684f5044555df5dc
saneef <hello@saneef.com>
2020-08-17 13:53:40 +0530

    ci(sentry): enables sourcemap rewrite

--- release-on-sentry.sh
+++ release-on-sentry.sh
@@ -3,5 +3,5 @@ RELEASE_NAME="$COMMIT_REF-$BUILD_ID"
 
 echo $RELEASE_NAME
 npx sentry-cli releases new $RELEASE_NAME
-npx sentry-cli releases files $RELEASE_NAME upload-sourcemaps public/
+npx sentry-cli releases files $RELEASE_NAME upload-sourcemaps public/ --rewrite
 npx sentry-cli releases finalize $RELEASE_NAME

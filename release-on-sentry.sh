379f456af1a1c13d1a8b45c38a9d5c5af45053eb
saneef <hello@saneef.com>
2020-08-18 17:01:13 +0530

    ci(sentry): disables release set-commits

--- release-on-sentry.sh
+++ release-on-sentry.sh
@@ -4,5 +4,5 @@ RELEASE_NAME=$(sentry-cli releases propose-version)
 echo "📦 Creating release: "$RELEASE_NAME
 npx sentry-cli releases new $RELEASE_NAME
 npx sentry-cli releases files $RELEASE_NAME upload-sourcemaps public/ --rewrite
-npx sentry-cli releases set-commits --auto $RELEASE_NAME
+# npx sentry-cli releases set-commits --auto $RELEASE_NAME
 npx sentry-cli releases finalize $RELEASE_NAME

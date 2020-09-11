d5d8d0653e56cfd4f5e830cb8ee4d93bbdcd26e4
saneef <hello@saneef.com>
2020-09-11 20:04:26 +0530

    adds FB and Twitter meta tags

--- html-template.js
+++ html-template.js
@@ -1,6 +1,12 @@
 import { makeHtmlAttributes } from "@rollup/plugin-html";
 
-export const template = async ({ attributes, files, publicPath, title }) => {
+export const template = async ({ attributes, files, publicPath }) => {
+  const title = "color × color";
+  const siteUrl = "https://colorcolor.in";
+  const description =
+    "Color-color is a tool to create accessible color systems for UIs. It uses HSLuv color space to create perceptually uniform colors.";
+  const imagePath = `${siteUrl}/screenshot.jpg`;
+
   const scripts = (files.js || [])
     .map(({ fileName }) => {
       const attrs = makeHtmlAttributes(attributes.script);
@@ -21,7 +27,22 @@ export const template = async ({ attributes, files, publicPath, title }) => {
     <meta charset="utf-8">
     <meta name=viewport content="width=device-width,initial-scale=1">
     <title>${title}</title>
-    <meta name="description" content="Color-color is a tool to create accessible color systems for UIs. It uses HSLuv color space to create perceptually uniform colors.">
+    <meta name="description" content="${description}">
+
+    <!-- Facebook Meta Tags -->
+    <meta property="og:url" content="${siteUrl}">
+    <meta property="og:type" content="website">
+    <meta property="og:title" content="${title}">
+    <meta property="og:description" content="${description}">
+    <meta property="og:image" content="${imagePath}">
+
+    <!-- Twitter Meta Tags -->
+    <meta property="twitter:url" content="${siteUrl}">
+    <meta name="twitter:card" content="summary_large_image">
+    <meta name="twitter:title" content="${title}">
+    <meta name="twitter:description" content="${description}">
+    <meta name="twitter:image" content="${imagePath}">
+
     ${links}
     ${scripts}
   </head>

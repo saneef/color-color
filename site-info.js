332e9c8ce69864fb1a83e4a16a96ee5e1a2e502c
saneef <hello@saneef.com>
2021-05-22 22:46:58 +0530

    fix: adds back twitter and open graph meta tags

---
+++ site-info.js
@@ -0,0 +1,11 @@
+const siteUrl = "https://colorcolor.in";
+
+const info = {
+  title: "color × color",
+  description:
+    "Color-color is a tool to create accessible color systems for UIs. It uses HSLuv color space to create perceptually uniform colors.",
+  siteUrl,
+  imagePath: `${siteUrl}/screenshot.jpg`,
+};
+
+export default info;
--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,16 @@
 import { defineConfig } from "vite";
 import svelte from "@sveltejs/vite-plugin-svelte";
 import { VitePWA } from "vite-plugin-pwa";
+import { injectHtml } from "vite-plugin-html";
+import siteInfo from "./site-info.js";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
+    injectHtml({
+      injectData: {
+        ...siteInfo,
+      },
+    }),
     svelte(),
     VitePWA({
       registerType: "autoUpdate",

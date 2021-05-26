3e7806cbb276c7421cd13e1c30ef58aa2cd677a2
saneef <hello@saneef.com>
2021-05-26 14:53:14 +0530

    chore(ava): replaces jest with ava as test runner
    Also, test on pre-commit

--- src/lib/eases.test.js
+++ src/lib/eases.test.js
@@ -1,3 +1,4 @@
+import test from "ava";
 import {
   getBezierEasingByAlias,
   getAliasByBezierEasing,
@@ -5,51 +6,42 @@ import {
   cubicBezierParamsToString,
 } from "./eases.js";
 
-describe("query eases", () => {
-  test("should get cubic bezier value for 'circInOut'", () => {
-    expect(getBezierEasingByAlias("circInOut")).toBe(".85,0,.15,1");
-  });
-
-  test("should not get cubic bezier value for 'blah'", () => {
-    expect(getBezierEasingByAlias("blah")).toBeUndefined();
-  });
-
-  test("should get cubic bezier alias for '.85,0,.15,1'", () => {
-    expect(getAliasByBezierEasing(".85,0,.15,1")).toBe("circInOut");
-  });
-
-  test("should get cubic bezier alias for '.85,0,.15,1.5'", () => {
-    expect(getAliasByBezierEasing(".85,0,.15,1.5")).toBeUndefined();
-  });
+test("getBezierEasingByAlias: should get cubic bezier value for 'circInOut'", (t) => {
+  t.truthy(getBezierEasingByAlias("circInOut") === ".85,0,.15,1");
 });
 
-describe("cubic bezier easing params", () => {
-  test("should parse", () => {
-    expect(stringToCubicBezierParams(".2,.2,.2,.2")).toStrictEqual([
-      0.2,
-      0.2,
-      0.2,
-      0.2,
-    ]);
-  });
+test("getBezierEasingByAlias: should not get cubic bezier value for 'blah'", (t) => {
+  t.truthy(getBezierEasingByAlias("blah") === undefined);
+});
 
-  test("should not parse", () => {
-    expect(stringToCubicBezierParams(".2,.2,.2")).toBeUndefined();
-  });
+test("getAliasByBezierEasing: should get cubic bezier alias for '.85,0,.15,1'", (t) => {
+  t.truthy(getAliasByBezierEasing(".85,0,.15,1") === "circInOut");
+});
 
-  test("should not parse this too", () => {
-    expect(stringToCubicBezierParams(".2,.2,.2,a")).toBeUndefined();
-  });
+test("getAliasByBezierEasing: should get cubic bezier alias for '.85,0,.15,1.5'", (t) => {
+  t.truthy(getAliasByBezierEasing(".85,0,.15,1.5") === undefined);
+});
 
-  test("should encode", () => {
-    expect(cubicBezierParamsToString([1, 0.2, 0.33, 0.2])).toBe("1,.2,.33,.2");
-  });
+test("should parse", (t) => {
+  t.deepEqual(stringToCubicBezierParams(".2,.2,.2,.2"), [0.2, 0.2, 0.2, 0.2]);
+});
 
-  test("should not encode", () => {
-    expect(cubicBezierParamsToString([0.2, 0.2, 0.2])).toBeUndefined();
-  });
+test("stringToCubicBezierParams: should not parse", (t) => {
+  t.truthy(stringToCubicBezierParams(".2,.2,.2") === undefined);
+});
 
-  test("should not encode this too", () => {
-    expect(cubicBezierParamsToString([0.2, 0.2, 0.2, "a"])).toBeUndefined();
-  });
+test("stringToCubicBezierParams: should not parse this too", (t) => {
+  t.truthy(stringToCubicBezierParams(".2,.2,.2,a") === undefined);
+});
+
+test("cubicBezierParamsToString: should encode", (t) => {
+  t.truthy(cubicBezierParamsToString([1, 0.2, 0.33, 0.2]) === "1,.2,.33,.2");
+});
+
+test("cubicBezierParamsToString: should not encode", (t) => {
+  t.truthy(cubicBezierParamsToString([0.2, 0.2, 0.2]) === undefined);
+});
+
+test("cubicBezierParamsToString: should not encode this too", (t) => {
+  t.truthy(cubicBezierParamsToString([0.2, 0.2, 0.2, "a"]) === undefined);
 });

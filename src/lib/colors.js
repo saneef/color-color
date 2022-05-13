9cd0ff0b6f4fb4826ab092209ad5211446687158
saneef <hello@saneef.com>
2022-05-13 17:32:30 +0530

    Use Okhsl color space when decoding reference colors

--- src/lib/colors.js
+++ src/lib/colors.js
@@ -1,11 +1,18 @@
 import hsluv from "hsluv";
 import chroma from "chroma-js";
 import { clamp } from "./math";
-import { okhsl_to_srgb } from "./ok-color";
+import { okhsl_to_srgb, srgb_to_okhsl } from "./ok-color";
 
 const okhslToHex = function (h, s, l) {
   const [r, g, b] = okhsl_to_srgb(h, s, l);
   return chroma.rgb(r, g, b).hex();
+};
+
+const hexToOklsh = function (hex) {
+  let [r, g, b] = chroma(hex).rgb();
+  let [angle, s, l] = srgb_to_okhsl(r, g, b);
+  let h = 360 * angle;
+  return [clamp(0, h, 360), clamp(0, s * 100, 100), clamp(0, l * 100, 100)];
 };
 
 export const hslToHex = function (h, s, l, colorSpace = "hsluv") {
@@ -25,6 +32,8 @@ export const hexToHsl = function (hex, colorSpace = "hsluv") {
     case "hsl":
       [h, s, l] = chroma(hex).hsl();
       return [h, s * 100, isNaN(l) ? 0 : l];
+    case "okhsl":
+      return hexToOklsh(hex);
     default:
       [h, s, l] = hsluv.hexToHsluv(hex);
       return [clamp(0, h, 360), clamp(0, s, 100), clamp(0, l, 100)];
